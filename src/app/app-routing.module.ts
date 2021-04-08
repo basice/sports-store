5284660981e72785e64f207848b578887dc4c1cb
Emir Basic <ebasic@gmail.com>
2021-04-08 17:36:04 +0200

    #22. Creating and Applying the Routing Configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from './store/store.module';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cart-detail/cart-detail.component';
+import { CheckoutComponent } from './store/checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'store', component: StoreComponent },
+  { path: 'cart', component: CartDetailComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: '**', redirectTo: '/store' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), StoreModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
