e145f8a0ce3c3e79aa1407c39107c117c93f08d2
Emir Basic <ebasic@gmail.com>
2021-04-08 18:43:44 +0200

    #28. Updating the Feature Module

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProductService } from './product.service';
 import { StaticDatasourceService } from './static-datasource.service';
 import { CartService } from './cart.service';
+import { Order } from './order.model';
+import { OrderService } from './order.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [ProductService, StaticDatasourceService, CartService],
+  providers: [ProductService, StaticDatasourceService, CartService, Order, OrderService],
 })
 export class ModelModule {}
