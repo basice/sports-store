b1056bdb97e643696b960b4adfccd8375b247c26
Emir Basic <ebasic@gmail.com>
2021-04-08 17:50:11 +0200

    #24. Guarding the Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { StoreModule } from './store/store.module';
 import { StoreComponent } from './store/store.component';
 import { CartDetailComponent } from './store/cart-detail/cart-detail.component';
 import { CheckoutComponent } from './store/checkout/checkout.component';
+import { StoreFirstGuard } from './store-first-guard.service';
 
 const routes: Routes = [
-  { path: 'store', component: StoreComponent },
-  { path: 'cart', component: CartDetailComponent },
-  { path: 'checkout', component: CheckoutComponent },
+  { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard] },
+  { path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard] },
+  { path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard] },
   { path: '**', redirectTo: '/store' },
 ];
 
