3f59296b1b670dfe86eef5a24ee11f768aa0e397
Emir Basic <ebasic@gmail.com>
2021-04-08 17:59:39 +0200

    #25. Completing the Cart Detail Feature

--- src/app/store/cart-detail/cart-detail.component.ts
+++ src/app/store/cart-detail/cart-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../model/cart.service';
 
 @Component({
   selector: 'app-cart-detail',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cart: CartService) { }
 
   ngOnInit(): void {
   }
