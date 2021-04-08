c32820a1812b85a08e7e36ddb3edfb603a9549f9
Emir Basic <ebasic@gmail.com>
2021-04-08 15:31:56 +0200

    #15. Adding Category Selection

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,15 +8,21 @@ import { ProductService } from '../model/product.service';
   styleUrls: ['./store.component.css'],
 })
 export class StoreComponent implements OnInit {
+  public selectedCategory = null;
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {}
 
   get products(): Product[] {
-    return this.productService.getProducts();
+    return this.productService.getProducts(this.selectedCategory);
   }
 
   get categories(): string[] {
     return this.productService.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
   }
 }
