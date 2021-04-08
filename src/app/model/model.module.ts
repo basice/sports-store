00c03589d7d3c7039a31f3f41f5c6d64ac845a52
Emir Basic <ebasic@gmail.com>
2021-04-08 17:21:00 +0200

    #18. Creating the Cart Model (Service)

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from './product.service';
 import { StaticDataSource } from './static.datasource';
+import { CartService } from './cart.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [ProductService, StaticDataSource],
+  providers: [ProductService, StaticDataSource, CartService],
 })
 export class ModelModule {}
