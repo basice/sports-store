15a0fa4eae3a181486347c9e2b15bf266af98b05
Emir Basic <ebasic@gmail.com>
2021-04-09 14:58:38 +0200

    #43. Implementing the Orders Feature

--- src/app/admin/order-table/order-table.component.ts
+++ src/app/admin/order-table/order-table.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../model/order.service';
+import { Order } from '../../model/order.model';
 
 @Component({
   selector: 'app-order-table',
-  templateUrl: './order-table.component.html'
+  templateUrl: './order-table.component.html',
 })
 export class OrderTableComponent implements OnInit {
+  includeShipped = false;
 
-  constructor() { }
+  constructor(private orderService: OrderService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getOrders(): Order[] {
+    return this.orderService.getOrders().filter((o) => this.includeShipped || !o.shipped);
   }
 
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.orderService.updateOrder(order);
+  }
+
+  delete(id: number) {
+    this.orderService.deleteOrder(id);
+  }
 }
