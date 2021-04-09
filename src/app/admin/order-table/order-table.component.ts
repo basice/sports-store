8db1a138747eb5ae22372873dbb5bcd0963108d3
Emir Basic <ebasic@gmail.com>
2021-04-09 13:45:34 +0200

    #39. Creating the Placeholder Components

---
+++ src/app/admin/order-table/order-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-table',
+  templateUrl: './order-table.component.html'
+})
+export class OrderTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/admin/product-table/product-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-table',
+  templateUrl: './product-table.component.html'
+})
+export class ProductTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
