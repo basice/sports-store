a55f2b406144833b21e3e13b98f4a847f97588d5
Emir Basic <ebasic@gmail.com>
2021-04-09 13:46:35 +0200

    #40. Preparing the Common Content and the Feature Module

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../model/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
 })
 export class AdminComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  logout() {
+    this.authService.clear();
+    this.router.navigateByUrl('/');
+  }
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,15 +5,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './auth.guard';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { ProductEditorComponent } from './product-editor/product-editor.component';
+import { OrderTableComponent } from './order-table/order-table.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'main', component: AdminComponent, canActivate: [AuthGuard] },
+  {
+    path: 'main',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'products/:mode/:id', component: ProductEditorComponent },
+      { path: 'products/:mode', component: ProductEditorComponent },
+      { path: 'products', component: ProductTableComponent },
+      { path: 'orders', component: OrderTableComponent },
+      { path: '**', redirectTo: 'products' },
+    ],
+  },
   { path: '**', redirectTo: 'auth' },
 ];
 
 @NgModule({
-  declarations: [AuthComponent, AdminComponent],
+  declarations: [AuthComponent, AdminComponent, ProductTableComponent, ProductEditorComponent, OrderTableComponent],
   imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
   providers: [AuthGuard],
 })
